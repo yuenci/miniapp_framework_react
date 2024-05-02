924da1cd9183597b9098e7ecf6a6e6e5db99f1f1
Innis <1575270674@qq.com>
2024-05-02 15:42:45 +0800

    feat: add share button

--- src/components/action-sheet-menu.jsx
+++ src/components/action-sheet-menu.jsx
@@ -1,15 +1,24 @@
-import {GoCommentDiscussion, GoGear, GoHeart, GoInfo, GoKebabHorizontal, GoTrash, GoX} from "react-icons/go";
+import config from "../../package.json";
+import {
+    GoCommentDiscussion,
+    GoGear,
+    GoHeart,
+    GoInfo,
+    GoPerson,
+    GoShareAndroid,
+    GoTrash,
+} from "react-icons/go";
 import {IoRefreshSharp} from "react-icons/io5";
 
 function ActionSheetButton({title, icon,onClick}){
 
     return (
         <div className={"flex flex-col items-center "}>
-            <div className={"flex flex-col items-center justify-center bg-gray-100 w-12 h-12 rounded-lg "}
+            <button className={"flex flex-col items-center justify-center bg-gray-100 w-12 h-12 rounded-lg cursor-pointer"}
                     onClick={onClick}
             >
                 {icon}
-            </div>
+            </button>
             <div className={"text-xs mt-1 text-gray-500"}>{title}</div>
         </div>
     )
@@ -31,6 +40,49 @@ export default  function ActionSheetMenu({showMenu,setShowMenu}){
         setShowMenu(false);
     }
 
+    function  copyToClipboard(){
+        navigator.clipboard.writeText(window.location.href);
+    }
+
+    const buttonsData = [
+        {
+            title: "Favorite",
+            icon: <GoHeart className={buttonStyle}/>
+        },
+        {
+            title: "Feedback",
+            icon: <GoCommentDiscussion className={buttonStyle}/>
+        },
+        {
+            title: "Re-enter",
+            icon: <IoRefreshSharp className={buttonStyle}/>,
+            onClick: handleRefresh
+        },
+        {
+            title: "Clean Cache",
+            icon: <GoTrash className={buttonStyle}/>
+        },
+        {
+            title: "Settings",
+            icon: <GoGear className={buttonStyle}/>
+        },
+        {
+            title: "Profile",
+            icon: <GoPerson className={buttonStyle}/>
+        },
+        {
+            title: "About",
+            icon: <GoInfo className={buttonStyle}/>,
+            onClick: goToAbout
+        },
+        {
+            title: "Share",
+            icon: config.shared ? <GoShareAndroid className={buttonStyle}/>
+                :<GoShareAndroid className={`${buttonStyle} text-gray-300`}/>,
+            onClick: config.shared ? copyToClipboard : null
+        }
+    ]
+
 
     return (
         <div
@@ -42,19 +94,11 @@ export default  function ActionSheetMenu({showMenu,setShowMenu}){
                     MiniApp Framework
                 </div>
                 <div className={"grid grid-cols-4 gap-2 p-4 w-full"}>
-                    <ActionSheetButton title={"Favorite"} icon={<GoHeart className={buttonStyle}/>}/>
-                    <ActionSheetButton title={"Feedback"} icon={<GoCommentDiscussion className={buttonStyle}/>}/>
-                    <ActionSheetButton title={"Re-enter"} icon={<IoRefreshSharp className={buttonStyle}/>}
-                                        onClick={handleRefresh}
-                    />
-                    <ActionSheetButton title={"Clean Cache"} icon={<GoTrash className={buttonStyle}/>}/>
-                    <ActionSheetButton title={"About"} icon={<GoInfo className={buttonStyle}/>}
-                                        onClick={goToAbout}
-                    />
-                    <ActionSheetButton title={"Settings"} icon={<GoGear className={buttonStyle}/>}/>
-                    <ActionSheetButton title={"Edit"} icon={<GoHeart className={buttonStyle}/>}/>
+                    {buttonsData.map((button, index) => (
+                        <ActionSheetButton key={index} title={button.title} icon={button.icon} onClick={button.onClick}/>
+                    ))}
                 </div>
-                <div className={"text-center p-5 border-t"} onClick={cancel}>
+                <div className={"text-center p-5 border-t cursor-pointer"} onClick={cancel}>
                     Cancel
                 </div>
             </div>
